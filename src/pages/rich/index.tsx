2b8e43ff7a3a75efce8998c7fd6297c204d475a6
yc08230823 <yc08230823@163.163.com>
2021-10-10 15:29:59 +0800

    富文本功能开发完毕

--- src/pages/rich/index.tsx
+++ src/pages/rich/index.tsx
@@ -64,3 +64,48 @@
 // };
 
 // export default RichPage;
+import { Card } from "antd"
+import   React,{useState,useEffect} from 'react';
+import BraftEditor from 'braft-editor'
+import 'braft-editor/dist/index.css'
+interface IRichPageProps {
+}
+
+
+const RichPage: React.FunctionComponent<IRichPageProps> = (props) => {
+    let [state,setState] = useState({
+        editorState: BraftEditor.createEditorState('<p>Hello <b>World!</b></p>'), 
+        outputHTML: '<p></p>'
+    })
+    let isLivinig:any
+    useEffect(() => {
+       isLivinig = true
+        // 3秒后更改编辑器内容
+        setTimeout(setEditorContentAsync, 3000)
+    }, []);
+   const handleChange = (editorState:any) => {
+       console.log("editorState",editorState.toHTML());
+        setState({
+          editorState: editorState,
+          outputHTML: editorState.toHTML()
+        })
+      }
+      
+      const setEditorContentAsync = () => {
+        isLivinig && setState({
+          editorState: BraftEditor.createEditorState('<p>你好，<b>世界!</b><p>'),
+          outputHTML: ''
+        })
+      }
+  return <div>
+      <Card title="富文本编辑器" >
+      <BraftEditor
+            value={state.editorState}
+            onChange={handleChange}
+          />
+       <div className="output-content">{state.outputHTML}</div>
+      </Card>
+  </div>;
+};
+
+export default RichPage;
